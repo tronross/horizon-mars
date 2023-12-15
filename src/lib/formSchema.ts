b288ec2ca8e28853a87cc81f137991558ce0b707
tronross <john_ross@analogforever.ca>
2023-12-15 14:23:31 -0500

    Remove date coercion; add dateRegex; refactor birthDate validation and processing

--- src/lib/formSchema.ts
+++ src/lib/formSchema.ts
@@ -1,16 +1,23 @@
+//////////////////////////////////
+// MarsVisitorForm - Form schema
+//////////////////////////////////
+
 import { z } from "zod";
 
+// Regex for date and phone number validation
+const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
 const phoneRegex = new RegExp(
   /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
 );
 
+// Form schema
 export const formSchema = z.object({
-  firstName: z.string().min(1,"First name is required"),
-  lastName: z.string().min(1,"Last name is required"),
-  birthDate: z.coerce.date({
-    required_error: "Birth date is required",
-    invalid_type_error: "Invalid date",
-  }),
+  firstName: z.string().min(1, "First name is required"),
+  lastName: z.string().min(1, "Last name is required"),
+  birthDate: z.string()
+    .regex(dateRegex, 'Birth date must be in the format yyyy-mm-dd')
+    .refine(dateString => !isNaN(Date.parse(dateString)), 'Invalid date')
+    .transform((val) => new Date(val)),
   nationality: z.string().min(1, "Nationality is required"),
   email: z.string().email("Please enter a valid email address"),
   phoneNumber: z.string().regex(phoneRegex, "Please enter a valid phone number"),
