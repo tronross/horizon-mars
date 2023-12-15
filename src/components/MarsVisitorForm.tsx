37c4d9b13b35305faf5265de5c119f8776195eaa
tronross <john_ross@analogforever.ca>
2023-12-15 10:11:13 -0500

    Add styling to form; import schema; format document

--- src/components/MarsVisitorForm.tsx
+++ src/components/MarsVisitorForm.tsx
@@ -3,13 +3,9 @@ import { useForm, SubmitHandler, type FieldValues } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-const schema = z.object({
-  firstName: z.string().min(1,"First name is required"),
-  lastName: z.string().min(1,"Last name is required"),
-  email: z.string().email(),
-});
+import { formSchema } from "@/lib/formSchema";
 
-type FormValues = z.infer<typeof schema>;
+type Inputs = z.infer<typeof formSchema>;
 
 export default function MarsVisitorForm() {
   const {
@@ -18,42 +14,47 @@ export default function MarsVisitorForm() {
     formState: { errors, isSubmitting },
     reset,
     getValues
-  } = useForm<FormValues>({
-    resolver: zodResolver(schema),
+  } = useForm<Inputs>({
+    resolver: zodResolver(formSchema),
   });
 
-  const onSubmit: SubmitHandler<FieldValues> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+    reset();
+  }
 
   return (
-    <form className="flex flex-col gap-y-2 max-w-screen-sm" onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        {...register("firstName")}
-        type="firstName"
-        placeholder="First Name"
-        className="px-2 py-2 rounded"
-      />
-      {errors.firstName && <p className="text-red-500">{errors.firstName.message}</p>}
+    <section className="flex flex-col items-center justify-center max-h-screen py-12">
+      <form className="flex flex-col gap-y-2 max-w-screen-sm" onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          {...register("firstName")}
+          type="firstName"
+          placeholder="First Name"
+          className="px-2 py-2 rounded text-black"
+        />
+        {errors.firstName && <p className="text-red-500">{errors.firstName.message}</p>}
 
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        {...register("lastName")}
-        type="lastName"
-        placeholder="Last Name"
-        className="px-2 py-2 rounded"
-      />
-      {errors.lastName && <p className="text-red-500">{errors.lastName.message}</p>}
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          {...register("lastName")}
+          type="lastName"
+          placeholder="Last Name"
+          className="px-2 py-2 rounded text-black"
+        />
+        {errors.lastName && <p className="text-red-500">{errors.lastName.message}</p>}
 
-      <label htmlFor="email">Email</label>
-      <input
-        {...register("email")}
-        type="email"
-        placeholder="Email"
-        className="px-2 py-2 rounded"
-      />
-      {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+        <label htmlFor="email">Email</label>
+        <input
+          {...register("email")}
+          type="email"
+          placeholder="Email"
+          className="px-2 py-2 rounded text-black"
+        />
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
 
-      <button type="submit">Submit</button>
-    </form>
+        <button type="submit" className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 border border-teal-700 rounded">Submit</button>
+      </form>
+    </section>
   );
 }
