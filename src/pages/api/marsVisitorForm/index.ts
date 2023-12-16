3e951b4077f61d6733a516808cf074a62feeb402
tronross <john_ross@analogforever.ca>
2023-12-16 10:11:58 -0500

    Add comments; remove dead and test code

--- src/pages/api/marsVisitorForm/index.ts
+++ src/pages/api/marsVisitorForm/index.ts
@@ -1,18 +1,23 @@
+///////////////////////////
+// marsVisitorForm Handler
+///////////////////////////
+
 import { formSchema } from '../../../lib/formSchema';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
+    // Validate the data, server-side
     const result = formSchema.safeParse(req.body);
 
     if (!result.success) {
-      // If validation fails, send a 400 Bad Request response with the validation errors
       res.status(400).json({ message: 'Invalid data', errors: result.error.errors });
       return;
     }
 
+    // Code to save the data to a database would go here,
+    // but we're just going to log it to the console as proof of concept
     console.log('Validated data:', result.data);
     res.status(200).json({ message: 'ok' });
-    console.log('You have reached POST api/marsVisitorForm')
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
   }
