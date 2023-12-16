b3d97c5bd3650563a6a8e400e4d5218b266ca45f
tronross <john_ross@analogforever.ca>
2023-12-16 15:17:16 -0500

    Fix form width consistency

--- src/components/MarsVisitorForm.tsx
+++ src/components/MarsVisitorForm.tsx
@@ -2,12 +2,12 @@
 // MarsVisitorForm component
 /////////////////////////////
 
-import { useState, useEffect } from "react";
-import { useForm, SubmitHandler, SubmitErrorHandler } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { useState, useEffect } from 'react';
+import { useForm, SubmitHandler, SubmitErrorHandler } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
-import { formSchema } from "@/lib/formSchema";
+import { formSchema } from '@/lib/formSchema';
 
 type Inputs = z.infer<typeof formSchema>;
 
@@ -134,61 +134,59 @@ export default function MarsVisitorForm() {
 
   // Render Component
   return (
-    // <section className="flex flex-col items-center justify-center max-h-screen py-12">
-    //   <form className="flex flex-col gap-y-2 max-w-screen-sm" onSubmit={handleSubmit(onSubmit, onError)}>
-    <section className="flex flex-col items-center justify-center py-12 px-4 sm:px-0">
-      <form className="flex flex-col gap-y-2 max-w-screen-sm mx-auto" onSubmit={handleSubmit(onSubmit, onError)}>
+    <section className="form-container flex flex-col items-center justify-center py-12 px-4 sm:px-0">
+      <form className="container flex flex-col gap-y-2 max-w-screen-sm mx-auto" onSubmit={handleSubmit(onSubmit, onError)}>
 
 
         {currentStage === 0 && (
           <>
-            <h2 className="text-2xl font-bold text-center">Personal Information</h2>
-            <label htmlFor="firstName">First Name</label>
+            <h2 className="text-2xl font-bold text-center max-w-screen-sm">Personal Information</h2>
+            <label className="max-w-screen-sm" htmlFor="firstName">First Name</label>
             <input
               {...register("firstName")}
               type="text"
-              className="px-2 py-2 rounded text-black" />
-            {errors.firstName && <p className="text-red-500">{errors.firstName.message}</p>}
+              className="px-2 py-2 rounded text-black w-full" />
+            {errors.firstName && <p className="text-red-500 max-w-screen-sm">{errors.firstName.message}</p>}
 
             <label htmlFor="lastName">Last Name</label>
             <input
               {...register("lastName")}
               type="text"
-              className="px-2 py-2 rounded text-black"
+              className="px-2 py-2 rounded text-black w-full"
             />
-            {errors.lastName && <p className="text-red-500">{errors.lastName.message}</p>}
+            {errors.lastName && <p className="text-red-500 max-w-screen-sm">{errors.lastName.message}</p>}
 
             <label htmlFor="birthDate">Date of Birth</label>
             <input
               {...register("birthDate")}
               type="date"
-              className="px-2 py-2 rounded text-black"
+              className="px-2 py-2 rounded text-black w-full"
             />
-            {errors.birthDate && <p className="text-red-500">{errors.birthDate.message}</p>}
+            {errors.birthDate && <p className="text-red-500 max-w-screen-sm">{errors.birthDate.message}</p>}
 
             <label htmlFor="nationality">Nationality</label>
             <input
               {...register("nationality")}
               type="text"
-              className="px-2 py-2 rounded text-black"
+              className="px-2 py-2 rounded text-black w-full"
             />
-            {errors.nationality && <p className="text-red-500">{errors.nationality.message}</p>}
+            {errors.nationality && <p className="text-red-500 max-w-screen-sm">{errors.nationality.message}</p>}
 
             <label htmlFor="email">Email</label>
             <input
               {...register("email")}
               type="email"
-              className="px-2 py-2 rounded text-black"
+              className="px-2 py-2 rounded text-black w-full"
             />
-            {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+            {errors.email && <p className="text-red-500 max-w-screen-sm">{errors.email.message}</p>}
 
             <label htmlFor="phoneNumber">Phone number</label>
             <input
               {...register("phoneNumber")}
               type="tel"
-              className="px-2 py-2 rounded text-black"
+              className="px-2 py-2 rounded text-black w-full"
             />
-            {errors.phoneNumber && <p className="text-red-500">{errors.phoneNumber.message}</p>}
+            {errors.phoneNumber && <p className="text-red-500 max-w-screen-sm">{errors.phoneNumber.message}</p>}
           </>
         )}
         {currentStage === 1 && (
