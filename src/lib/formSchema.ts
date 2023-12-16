283f7e1b2ed1ade7800091bf47b9a714daa41499
tronross <john_ross@analogforever.ca>
2023-12-16 15:20:29 -0500

    Refactor schema and add comments

--- src/lib/formSchema.ts
+++ src/lib/formSchema.ts
@@ -9,25 +9,22 @@ const phoneRegex = new RegExp(
   /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
 );
 
-// Form schema
+// Form schema - Error Messages also serve as explanatory comments for the schema
 export const formSchema = z.object({
   firstName: z.string().min(1, 'First name is required'),
   lastName: z.string().min(1, 'Last name is required'),
   birthDate: z.string()
     .refine(dateString => !isNaN(Date.parse(dateString)), 'Invalid date')
-    .refine(dateString => new Date(dateString) < new Date(), 'Birth date must be in the past')
-    .transform((val) => new Date(val)),
+    .refine(dateString => new Date(dateString) < new Date(), 'Birth date must be in the past'),
   nationality: z.string().min(1, 'Nationality is required'),
   email: z.string().email('Please enter a valid email address'),
   phoneNumber: z.string().regex(phoneRegex, 'Please enter a valid phone number'),
   departureDate: z.string()
     .refine(dateString => !isNaN(Date.parse(dateString)), 'Invalid date')
-    .refine(dateString => new Date(dateString) > new Date(), 'Launch date must be in the future')
-    .transform((val) => new Date(val)),
-    returnDate: z.string()
-    .refine(dateString => !isNaN(Date.parse(dateString)), 'Invalid date')
-    .transform((val) => new Date(val)),
-  departureHub: z.string().min(1, 'Departure hub is required'),
+    .refine(dateString => new Date(dateString) > new Date(), 'Launch Date must be in the future'),
+  returnDate: z.string()
+    .refine(dateString => !isNaN(Date.parse(dateString)), 'Invalid date'),
+  departureHub: z.string().min(1, 'Departure Hub is required'),
   martianLodgings: z.string().min(1, 'Martian Lodgings is required'),
   additionalNotes: z.string().optional(),
   healthDeclaration: z.boolean().optional().default(false).refine(val => val, 'You must Affirm to continue'),
@@ -35,4 +32,13 @@ export const formSchema = z.object({
   emergencyContactEmail: z.string().email('Please enter a valid email address'),
   emergencyContactPhone: z.string().regex(phoneRegex, 'Please enter a valid phone number'),
   medicalConditions: z.string().optional(),
-});
+}).refine( // Date comparison needs access to the whole object
+  ({ departureDate, returnDate }) => {
+    const departure = new Date(departureDate);
+    const returnD = new Date(returnDate);
+    const fourWeeksAfterDeparture = new Date(departure);
+    fourWeeksAfterDeparture.setDate(fourWeeksAfterDeparture.getDate() + 28);
+    return returnD >= fourWeeksAfterDeparture;
+  },
+  { message: 'Return Date must be at least 4 weeks after the Launch Date', path: ['returnDate'] }
+);
