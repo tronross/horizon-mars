18ebf2bd1bad03345fc438fe70a157cb3da8b99f
tronross <john_ross@analogforever.ca>
2023-12-18 14:38:23 -0500

    Delete redundant code exported to TravelPreferencesStage

--- src/components/MarsVisitorForm.tsx
+++ src/components/MarsVisitorForm.tsx
@@ -167,49 +167,7 @@ export default function MarsVisitorForm() {
         {currentStage === 0 && (<PersonalInformationStage register={register} errors={errors} />)}
 
         {currentStage === 1 && (<TravelPreferencesStage register={register} errors={errors} launchPads={launchPads} marsLodgings={marsLodgings} />)}
-        {/*  // <>
-          //   <h2 className="text-2xl font-bold text-center">Travel Preferences</h2>
-          //   <label htmlFor="departureDate">Launch Date</label>
-          //   <p>Please book at least 48hrs in advance of your flight.</p>
-          //   <input
-              {...register("departureDate")}
-              type="date"
-              className="px-2 py-2 rounded text-black" />
-            {errors.departureDate && <p className="text-red-500">{errors.departureDate.message}</p>}
-            <label htmlFor="returnDate">Return Date</label>
-            <input
-              {...register("returnDate")}
-              type="date"
-              className="px-2 py-2 rounded text-black" />
-            {errors.returnDate && <p className="text-red-500">{errors.returnDate.message}</p>}
-            <label htmlFor="departureHub">Launch Pad</label>
-            <select {...register("departureHub")} className="px-2 py-2 rounded text-black">
-              <option value="">Select a Departure Hub</option>
-              {launchPads.map((launchPad, index) => (
-                <option key={index} value={launchPad}>
-                  {launchPad}
-                </option>
-              ))}
-            </select>
-            {errors.departureHub && <p className="text-red-500">{errors.departureHub.message}</p>}
-            <label htmlFor="martianLodgings">Martian Lodgings</label>
-            <select {...register("martianLodgings")} className="px-2 py-2 rounded text-black">
-              <option value="">Select your Martian Lodgings</option>
-              {marsLodgings.map((marsLodging, index) => (
-                <option key={index} value={marsLodging}>
-                  {marsLodging}
-                </option>
-              ))}
-            </select>
-            {errors.martianLodgings && <p className="text-red-500">{errors.martianLodgings.message}</p>}
-            <label htmlFor="additionalNotes">Additional Notes</label>
-            <textarea
-              {...register("additionalNotes")}
-              className="px-2 py-4 rounded text-black" />
-            {errors.additionalNotes && <p className="text-red-500">{errors.additionalNotes.message}</p>}
-
-          </>} */}
-
+        
         {currentStage === 2 && (
           <>
             <h2 className="text-2xl font-bold text-center">Health and Safety</h2>
