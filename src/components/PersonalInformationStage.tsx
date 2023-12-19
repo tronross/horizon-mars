f67880028cbbcc741bd813076bc6b12657f29bb3
tronross <john_ross@analogforever.ca>
2023-12-19 13:14:39 -0500

    Add ids to PersonalInformationStage inputs

--- src/components/PersonalInformationStage.tsx
+++ src/components/PersonalInformationStage.tsx
@@ -16,12 +16,14 @@ export default function PersonalInformationStage({ register, errors }: PersonalI
       <h3 className="text-2xl font-bold text-center max-w-screen-sm">Personal Information</h3>
       <label className="max-w-screen-sm" htmlFor="firstName">First Name</label>
       <input
+        id="firstName"
         {...register("firstName")}
         type="text"
         className="px-2 py-2 rounded text-black w-full" />
       {errors.firstName && <p className="text-red-500 max-w-screen-sm">{errors.firstName.message}</p>}
       <label htmlFor="lastName">Last Name</label>
       <input
+        id="lastName"
         {...register("lastName")}
         type="text"
         className="px-2 py-2 rounded text-black w-full"
@@ -29,6 +31,7 @@ export default function PersonalInformationStage({ register, errors }: PersonalI
       {errors.lastName && <p className="text-red-500 max-w-screen-sm">{errors.lastName.message}</p>}
       <label htmlFor="birthDate">Date of Birth</label>
       <input
+        id="birthDate"
         {...register("birthDate")}
         type="date"
         className="px-2 py-2 rounded text-black w-full"
@@ -36,6 +39,7 @@ export default function PersonalInformationStage({ register, errors }: PersonalI
       {errors.birthDate && <p className="text-red-500 max-w-screen-sm">{errors.birthDate.message}</p>}
       <label htmlFor="nationality">Nationality</label>
       <input
+        id="nationality"
         {...register("nationality")}
         type="text"
         className="px-2 py-2 rounded text-black w-full"
@@ -43,6 +47,7 @@ export default function PersonalInformationStage({ register, errors }: PersonalI
       {errors.nationality && <p className="text-red-500 max-w-screen-sm">{errors.nationality.message}</p>}
       <label htmlFor="email">Email</label>
       <input
+        id="email"
         {...register("email")}
         type="email"
         className="px-2 py-2 rounded text-black w-full"
@@ -50,6 +55,7 @@ export default function PersonalInformationStage({ register, errors }: PersonalI
       {errors.email && <p className="text-red-500 max-w-screen-sm">{errors.email.message}</p>}
       <label htmlFor="phoneNumber">Phone number</label>
       <input
+        id="phoneNumber"
         {...register("phoneNumber")}
         type="tel"
         className="px-2 py-2 rounded text-black w-full"
