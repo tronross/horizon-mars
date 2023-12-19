8042e3dcab787a168ce183691e97a6d85e3dc519
tronross <john_ross@analogforever.ca>
2023-12-19 13:18:52 -0500

    Add jest tests for PersonalInformationStage

--- src/components/PesonalInformationStage.test.tsx
+++ src/components/PesonalInformationStage.test.tsx
@@ -12,3 +12,43 @@ import PersonalInformationStage from './PersonalInformationStage';
 // Define mock props
 const mockRegister = jest.fn();
 
+describe('PersonalInformationStage', () => {
+  const errors = {};
+
+  beforeEach(() => {
+    render(
+      <PersonalInformationStage
+        register={mockRegister}
+        errors={errors}
+      />
+    );
+  });
+
+  it('renders the title', () => {
+    expect(screen.getByText('Personal Information')).toBeInTheDocument();
+  });
+
+  it('renders the first name input', () => {
+    expect(screen.getByLabelText('First Name')).toBeInTheDocument();
+  });
+
+  it('renders the last name input', () => {
+    expect(screen.getByLabelText('Last Name')).toBeInTheDocument();
+  });
+
+  it('renders the date of birth input', () => {
+    expect(screen.getByLabelText('Date of Birth')).toBeInTheDocument();
+  });
+
+  it('renders the nationality input', () => {
+    expect(screen.getByLabelText('Nationality')).toBeInTheDocument();
+  });
+
+  it('renders the email input', () => {
+    expect(screen.getByLabelText('Email')).toBeInTheDocument();
+  });
+
+  it('renders the phone number input', () => {
+    expect(screen.getByLabelText('Phone number')).toBeInTheDocument();
+  });
+});
