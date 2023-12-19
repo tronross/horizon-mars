68053d348790a4149b0659ab79155232a001ea72
tronross <john_ross@analogforever.ca>
2023-12-19 13:07:47 -0500

    Add jest tests for TravelPreferencesStage

---
+++ src/components/TravelPreferencesStage.test.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { formSchema } from '../lib/formSchema';
+type Inputs = z.infer<typeof formSchema>;
+
+import TravelPreferencesStage from './TravelPreferencesStage';
+
+// Define mock props
+const mockRegister = jest.fn();
+
+describe('TravelPreferencesStage', () => {
+  const errors = {};
+  const launchPads = ['Launch Pad 1', 'Launch Pad 2'];
+  const marsLodgings = ['Lodging 1', 'Lodging 2'];
+
+  beforeEach(() => {
+    render(
+      <TravelPreferencesStage
+        register={mockRegister}
+        errors={errors}
+        launchPads={launchPads}
+        marsLodgings={marsLodgings}
+      />
+    );
+  });
+
+  it('renders the title', () => {
+    expect(screen.getByText('Travel Preferences')).toBeInTheDocument();
+  });
+
+  it('renders the departure date input', () => {
+    expect(screen.getByLabelText('Launch Date')).toBeInTheDocument();
+  });
+
+  it('renders the return date input', () => {
+    expect(screen.getByLabelText('Return Date')).toBeInTheDocument();
+  });
+
+  it('renders the departure hub select', () => {
+    expect(screen.getByLabelText('Launch Pad')).toBeInTheDocument();
+  });
+
+  it('renders the martian lodgings select', () => {
+    expect(screen.getByLabelText('Martian Lodgings')).toBeInTheDocument();
+  });
+
+  it('renders the additional notes textarea', () => {
+    expect(screen.getByLabelText('Additional Notes')).toBeInTheDocument();
+  });
+
+  it('calls register with the correct arguments for departure date', () => {
+    expect(mockRegister).toHaveBeenCalledWith('departureDate');
+  });
+
+  it('calls register with the correct arguments for return date', () => {
+    expect(mockRegister).toHaveBeenCalledWith('returnDate');
+  });
+
+  it('calls register with the correct arguments for departure hub', () => {
+    expect(mockRegister).toHaveBeenCalledWith('departureHub');
+  });
+
+  it('calls register with the correct arguments for martian lodgings', () => {
+    expect(mockRegister).toHaveBeenCalledWith('martianLodgings');
+  });
+
+  it('calls register with the correct arguments for additional notes', () => {
+    expect(mockRegister).toHaveBeenCalledWith('additionalNotes');
+  });
+
+  it('displays error messages when errors are present', () => {
+    const errors = {
+      departureDate: { message: 'Departure date error' },
+      returnDate: { message: 'Return date error' },
+      departureHub: { message: 'Departure hub error' },
+      martianLodgings: { message: 'Martian lodgings error' },
+      additionalNotes: { message: 'Additional notes error' },
+    };
+
+    render(
+      <TravelPreferencesStage
+        register={mockRegister}
+        errors={errors}
+        launchPads={launchPads}
+        marsLodgings={marsLodgings}
+      />
+    );
+
+    expect(screen.getByText('Departure date error')).toBeInTheDocument();
+    expect(screen.getByText('Return date error')).toBeInTheDocument();
+    expect(screen.getByText('Departure hub error')).toBeInTheDocument();
+    expect(screen.getByText('Martian lodgings error')).toBeInTheDocument();
+    expect(screen.getByText('Additional notes error')).toBeInTheDocument();
+  });
+});
